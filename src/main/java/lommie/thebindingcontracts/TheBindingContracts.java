56310c719727248e2eb114260f5e521727636870
tommyrobot666 <tommyrobot10@gmail.com>
2025-11-15 02:04:37 -0800

    fix bugs found in testing and add recipe

--- src/main/java/lommie/thebindingcontracts/TheBindingContracts.java
+++ src/main/java/lommie/thebindingcontracts/TheBindingContracts.java
@@ -2,16 +2,15 @@ package lommie.thebindingcontracts;
 
 import lommie.thebindingcontracts.items.ModItemComponents;
 import lommie.thebindingcontracts.items.ModItems;
-import lommie.thebindingcontracts.recipes.ModRecipeSerializers;
 import net.fabricmc.api.ModInitializer;
 
 public class TheBindingContracts implements ModInitializer {
+    // https://misode.github.io/recipe/
     public static final String MOD_ID = "thebindingcontracts";
 
     @Override
     public void onInitialize() {
         ModItemComponents.register();
         ModItems.register();
-        ModRecipeSerializers.register();
     }
 }
