c37bc57a78fb321f824c12b607035742f9a52813
tommyrobot666 <tommyrobot10@gmail.com>
2025-11-21 03:00:44 -0800

    add onUseWhenOtherIsOnline and onTickForEachPlayer to TermsAndConditions add lots of stuff to Contract

--- src/main/java/lommie/thebindingcontracts/contract/Contract.java
+++ src/main/java/lommie/thebindingcontracts/contract/Contract.java
@@ -1,13 +1,75 @@
 package lommie.thebindingcontracts.contract;
 
+import lommie.thebindingcontracts.items.ContractItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Contract {
-    public ArrayList<TermsAndConditions> terms = new ArrayList<>();
+    private final ArrayList<TermsAndConditions> terms = new ArrayList<>();
+    private final ArrayList<UUID> signers = new ArrayList<>();
+    public boolean signed = false;
 
     public Contract(){}
 
-    public void onUseItem(int selectedTerm) {
-        terms.get(selectedTerm);
+    public void onUseItem(int selectedTerm, World world, PlayerEntity user, Hand hand) {
+        if(!signers.contains(user.getUuid())) return;
+        if(world.isClient()) return;
+        ItemStack stack = user.getStackInHand(hand);
+        ItemStack stackInOtherHand = inOtherHand(user, hand);
+        UUID otherId = ContractItem.getOtherPlayer(stack, user.getUuid());
+        PlayerEntity other = world.getPlayerAnyDimension(otherId);
+
+
+        TermsAndConditions term = terms.get(selectedTerm);
+
+        // bunch of other onUse stuff
+
+        if (other == null) return;
+        
+        term.onUseWhenOtherIsOnline((ServerWorld) world, (ServerPlayerEntity) user, (ServerPlayerEntity) other,hand,stack,stackInOtherHand,this);
+    }
+
+    private static ItemStack inOtherHand(PlayerEntity player, Hand hand){
+        if (hand == Hand.MAIN_HAND){
+            return player.getStackInHand(Hand.OFF_HAND);
+        } else {
+            return player.getStackInHand(Hand.MAIN_HAND);
+        }
+    }
+    
+    public void onTick(World world){
+        if (world.isClient()) return;
+        for (UUID uuid : signers){
+            PlayerEntity player = world.getPlayerAnyDimension(uuid);
+            if (player == null){
+                continue;
+            }
+            for (TermsAndConditions term : terms) {
+                term.onTickForEachPlayer((ServerWorld) world, (ServerPlayerEntity) player);
+            }
+        }
+    }
+
+    public void onTermsJustBroken(){}
+
+    public void onTermsBroken(){}
+
+    public void addTerm(TermsAndConditions term){
+        terms.add(term);
+    }
+
+    public void addSigner(UUID uuid){
+        signers.add(uuid);
+    }
+
+    public void sign(){
+        signed = true;
     }
 }
--- src/main/java/lommie/thebindingcontracts/contract/TermsAndConditions.java
+++ src/main/java/lommie/thebindingcontracts/contract/TermsAndConditions.java
@@ -1,4 +1,16 @@
 package lommie.thebindingcontracts.contract;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
+
 public class TermsAndConditions {
+    public void onUseWhenOtherIsOnline(ServerWorld world, ServerPlayerEntity user, ServerPlayerEntity other, Hand hand, ItemStack stack, ItemStack stackInOtherHand, Contract contract) {
+
+    }
+
+    public void onTickForEachPlayer(ServerWorld world, ServerPlayerEntity player){
+
+    }
 }
