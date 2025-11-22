b6b5625d85a76e0709459625bde2577f1091b63d
tommyrobot666 <tommyrobot10@gmail.com>
2025-11-21 22:33:08 -0800

    make new codec for TermsAndConditions

--- src/main/java/lommie/thebindingcontracts/TheBindingContracts.java
+++ src/main/java/lommie/thebindingcontracts/TheBindingContracts.java
@@ -1,8 +1,14 @@
 package lommie.thebindingcontracts;
 
+import lommie.thebindingcontracts.contract.TermsAndConditions;
 import lommie.thebindingcontracts.items.ModItemComponents;
 import lommie.thebindingcontracts.items.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.SimpleRegistry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +20,13 @@ public class TheBindingContracts implements ModInitializer {
     // https://misode.github.io/recipe/
     public static final String MOD_ID = "thebindingcontracts";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final RegistryKey<Registry<TermsAndConditions>> TERM_TYPE_REGISTRY_KEY = RegistryKey.ofRegistry(
+            Identifier.of(MOD_ID,"term_type")
+    );
+    public static final SimpleRegistry<TermsAndConditions> TERM_TYPE_REGISTRY = FabricRegistryBuilder.createSimple(
+            TERM_TYPE_REGISTRY_KEY
+    ).buildAndRegister();
 
     public static final List<UUID> playersToKill = new ArrayList<>();
 
--- src/main/java/lommie/thebindingcontracts/contract/TermsAndConditions.java
+++ src/main/java/lommie/thebindingcontracts/contract/TermsAndConditions.java
@@ -1,27 +1,44 @@
 package lommie.thebindingcontracts.contract;
 
-import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import lommie.thebindingcontracts.TheBindingContracts;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 
 public class TermsAndConditions {
-    public static final Codec<TermsAndConditions> CODEC = new CustomCodec();
-    public final NbtCompound savedData;
-    public final RegistryKey<TermsAndConditions> key;
+    public static final Codec<TermsAndConditions> CODEC = RecordCodecBuilder.create((instance) ->
+            instance.group(Identifier.CODEC.fieldOf("id")
+                                    .forGetter((t) -> t.key),
+                            NbtCompound.CODEC.fieldOf("savedData").forGetter((t) -> t.savedData))
+                    .apply(instance, (id,savedData) ->
+                        Objects.requireNonNull(TheBindingContracts.TERM_TYPE_REGISTRY.get(id)).newWithData(savedData)
+                    )
+    );
 
-    public TermsAndConditions(RegistryKey<TermsAndConditions> key) {
+    private TermsAndConditions newWithData(NbtCompound savedData) {
+        try {
+            return this.getClass().getConstructor(Identifier.class, NbtCompound.class).newInstance(this.key, savedData);
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public final NbtCompound savedData;
+    public final Identifier key;
+
+    public TermsAndConditions(Identifier key) {
         this(key, new NbtCompound());
     }
 
-    public TermsAndConditions(RegistryKey<TermsAndConditions> key, NbtCompound savedData) {
+    public TermsAndConditions(Identifier key, NbtCompound savedData) {
         this.key = key;
         this.savedData = savedData;
     }
@@ -32,26 +49,5 @@ public class TermsAndConditions {
 
     public void onTickForEachPlayer(ServerWorld world, ServerPlayerEntity player){
 
-    }
-
-    public Codec<TermsAndConditions> getCodec() {
-        return RecordCodecBuilder.create((instance) ->
-                instance.group(RegistryKey.createCodec(key.getRegistryRef()).fieldOf("id")
-                        .forGetter((t) -> t.key),
-                        NbtCompound.CODEC.fieldOf("savedData").forGetter((t) -> t.savedData))
-                        .apply(instance, TermsAndConditions::new)
-        );
-    }
-
-    private static class CustomCodec implements Codec<TermsAndConditions> {
-        @Override
-        public <T> DataResult<T> encode(TermsAndConditions termsAndConditions, DynamicOps<T> ops, T t) {
-            return termsAndConditions.getCodec().encode(termsAndConditions, ops, t);
-        }
-
-        @Override
-        public <T> DataResult<Pair<TermsAndConditions, T>> decode(DynamicOps<T> ops, T t) {
-            return null;
-        }
     }
 }
